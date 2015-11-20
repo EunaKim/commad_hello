255cfd342df35b2a73a75a6befbd310b391946ec
EunaKim <asdanny@naver.com>
2015-11-20 14:19:28 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,7 @@
 //For command line git_ex
 //GitHub modification : First
 //Local repository modification : Second
+//github test 
 println("Hello World")
 println("Hello pyo sun young")
 println("Hello your name")
